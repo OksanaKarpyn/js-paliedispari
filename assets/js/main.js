24f7f76801d29c136ff3e948ac446d313bc678fa
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-30 17:55:26 +0200

    agg pari dispari

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,86 @@
-// richiesta
-let promptString = prompt('inserisci parola palindroma');
+// // richiesta
+// let promptString = prompt('inserisci parola palindroma');
 
-//creo funzione
-function palindroma() {
-    // converte string in array
-    let string =promptString.split('');
+// //creo funzione
+// function palindroma() {
+//     // converte string in array
+//     let string = promptString.split('');
 
-    // rovescia la string .creo una let
-    let revString =string.reverse();
+//     // rovescia la string .creo una let
+//     let revString = string.reverse();
 
-    // converto  string che era diventata array di nuovo in string. creo una let
-    let newString = revString.join('');
+//     // converto  string che era diventata array di nuovo in string. creo una let
+//     let newString = revString.join('');
 
-    // condizione
-    if (newString == promptString ) {
+//     // condizione
+//     if (newString == promptString) {
 
-        prompt `la parola${promptString} è polindroma`;
-    } else {
-        prompt ` la parola ${promptString} non è polindroma`;
+//         prompt `la parola${promptString} è palindroma`;
+//     } else {
+//         prompt ` la parola ${promptString} non è palindroma`;
 
-    }
+//     }
+// }
+// palindroma();
+
+
+
+
+
+
+// ----pari dispari-----
+let number1;
+let number2;
+let output = document.querySelector('.out');
+let select = document.querySelector('#select');
+// function calcolo
+function sommaNumeri(a, b) {
+
+    // output.innerHTML=`${a + b}`;
+
+    return `${a + b}`
 }
-palindroma();
+
+
+let btn = document.querySelector('.btn').addEventListener('click', function () {
+    // prende valore che incesisce utente
+    number1 = parseInt(document.querySelector('#number1').value);
+    number2 = numRandom(1, 5);
+    // entra dentro funzione e fa calcolo
+    let somma = sommaNumeri(number1, number2)
+    // dopo calcolo asegna il valore alla funzione tramite return
+    // ouput dove voglio stampare e assegno funzione con valore
+    output.innerHTML = `${somma}`
+    select = select.value;
+    console.log(select);
+    if (somma % 2 == 0 && select == 'pari') {
+        output.innerHTML = `${somma} ${select} e pari hai vinto`
+
+    } else if (somma % 2 == 0 && select == 'dispari') {
+        output.innerHTML = `${somma} ${select}e pari hai perso`
+
+    } else if (somma % 2 != 0 && select == 'dispari') {
+        output.innerHTML = `${somma} ${select}e dispari hai vinto`
+
+    } else if (somma % 2 != 0 && select == 'pari') {
+        output.innerHTML = `${somma} ${select}e dispari hai perso`
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// random Function
+function numRandom(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
